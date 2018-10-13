ff871556e5af073dcc1674aa65ee87b20d308f13
Yusuke Hirata <yskhira.6661style@gmail.com>
2018-10-13 21:14:18 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CLIP_TEXTER
-DBからURLを取ってきて、そのURLのPDFからテキストを抽出し、ESにPUTする。
+## future spec
+1. get URL of PDF from DB
+2. convert from PDF to text
+3. put the text into ES
+
+## current spec
+- convert from PDF to text
+  - put PDF file to be converted into source folder.
+  - kick texter.sh with following parameters.
+      - s: source PDF file
+      - d: destination text file (will be created if it does not exit)
+
+## author
+- hirata_y
---
+++ shell/texter.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "[INFO] starging texter..."
+
+while getopts s:d: OPT
+do
+   case $OPT in
+     s) SRC=$OPTARG ;; 
+     d) DEST=$OPTARG ;;
+   esac
+done
+echo "[INFO] s arg: ${SRC}"
+echo "[INFO] d arg: ${DEST}"
+
+# source PDF check
+echo "[INFO] starting source PDF check..."
+echo "[INFO] source PDF: ${SRC}"
+SDIR="/home/hirata_y/texter/resource/sourcePdf/${SRC}"
+if [ -f ${SDIR} ]; then
+   echo "[INFO] source PDF found"
+else
+   echo "[ERROR] source PDF not found"
+   exit 1
+fi
+echo "[INFO] finished source PDF check"
+
+# destination file check
+echo "[INFO] starting destination file check..."
+echo "[INFO] destination file: ${DEST}"
+DDIR="/home/hirata_y/texter/resource/outputText/${DEST}"
+if [ -f ${DDIR} ]; then
+   echo "[INFO] destination file found"
+else
+   echo "[WARN] destination file does not exit"
+   touch ${DDIR}
+   echo "[INFO] created destination file"
+fi
+echo "[INFO] finished destination file check"
+
+# converting from PDF to text
+echo "[INFO] converting from PDF to text..."
+python /home/hirata_y/texter/python/texter.py ${SDIR} ${DDIR}
+echo "[INFO] finished converting"
+exit 0
